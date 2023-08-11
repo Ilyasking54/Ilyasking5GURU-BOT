e11c8f8102e2c8e9ed345da3486ac55b1faab694
Ilyas <141616481+Ilyasking54@users.noreply.github.com>
2023-08-11 17:33:15 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['03259298576', 'GURU', true],
+  [`03259298576', 'ilyasbaloch', true], 
   [''] 
 ] //Number of owners
 
